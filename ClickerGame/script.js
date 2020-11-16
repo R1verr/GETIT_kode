569be95d5d37760db8556547b550b7f5da3b0319
R1verr <74535816+R1verr@users.noreply.github.com>
2020-11-16 11:10:36 +0100

    Update script.js

--- ClickerGame/script.js
+++ ClickerGame/script.js
@@ -6,5 +6,24 @@ document.getElementById('current-count').innerHTML = pointsValue + " points!";
 function humanClicker() {
     pointsValue+=powerLevel;
     document.getElementById('current-count').innerHTML = pointsValue + " points!";
-
+    
+        if (pointsValue === 10) {
+        powerLevel += 1;
+        document.getElementById('power-up').innerHTML = 'Current powerlevel ' + powerLevel + ', so strong!';
+    }
+   
+    if (pointsValue === 100) {
+        powerLevel += 3;
+        document.getElementById('power-up').innerHTML = 'Current powerlevel ' + powerLevel + ', keep at it!';
+    } 
+    
+    if (pointsValue === 1000) {
+        powerLevel += 95;
+        document.getElementById('power-up').innerHTML = 'Current powerlevel ' + powerLevel + ', super strength!';
+    }    
+    
+    if (pointsValue === 10000) {
+        powerLevel ++;
+        document.getElementById('power-up').innerHTML = "Wow, you're still here?";
+    }
 }
